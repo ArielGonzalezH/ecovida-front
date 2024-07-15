4499bfc39b05444797ac1790b303395d58cef334
MMartinHan <mmhan@espe.edu.ec>
2024-07-15 09:44:35 -0500

    client_components

---
+++ ecovida-front/src/pages/find_fundation.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const FindFundation = () => {
+    return (
+        <div>
+            <h1>Find Fundation</h1>
+        </div>
+    )
+}
+
+export default FindFundation;
